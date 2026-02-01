cc3ae98671635d6abac2ff10f149e6697adff1f5
jasonn361 <79726650+jasonn361@users.noreply.github.com>
2026-01-31 21:52:19 -0600

    Updated Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,15 +10,20 @@ function Skills() {
         <SkillItem icon="devicon-cplusplus-plain" skill="C++"/>
         <SkillItem icon="devicon-html5-plain" skill="HTML"/>
         <SkillItem icon="devicon-css3-plain" skill="CSS"/>
-        <SkillItem icon="devicon-java-plain" skill="Java"/>
-        <SkillItem icon="devicon-javascript-plain" skill="JavaScript"/>
-        <SkillItem icon="devicon-sqlite-plain" skill="SQLite"/>
         <SkillItem icon="devicon-python-plain" skill="Python"/>
+        <SkillItem icon="devicon-javascript-plain" skill="JavaScript"/>
+        <SkillItem icon="devicon-java-plain" skill="Java"/>
+        <SkillItem icon="devicon-azureslqdatabase-plain" skill="SQL"/>
         <SkillItem icon="devicon-git-plain" skill="Git"/>
         <SkillItem icon="devicon-react-original" skill="React"/>
         <SkillItem icon="devicon-django-plain" skill="Django"/>
-        <SkillItem icon="devicon-hadoop-plain" skill="Hadoop"/>
-        <SkillItem icon="devicon-apachespark-original" skill="Spark"/>
+        <SkillItem icon="devicon-google-plain" skill="Google Test"/>
+        <SkillItem icon="" skill="Valgrind"/>
+        <SkillItem icon="devicon-pytest-plain" skill="Pytest"/>
+        <SkillItem icon="devicon-supabase-plain" skill="Supabase"/>
+        <SkillItem icon="devicon-mysql-original" skill="MySQL"/>
+        <SkillItem icon="devicon-expressjs-plain" skill="Express.js"/>
+        <SkillItem icon="devicon-nodejs-plain" skill="Node.js"/>
       </div>
     </div>
   );
